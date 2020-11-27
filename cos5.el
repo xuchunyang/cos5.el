60ca136dfbc8c6f6171aa67266099ce33122bc11
Xu Chunyang <xuchunyang56@gmail.com>
2020-11-27 17:31:12 +0800

    Add docstring to make eldoc happy

--- cos5.el
+++ cos5.el
@@ -34,12 +34,15 @@
 ;;;; 请求签名 https://cloud.tencent.com/document/product/436/7778
 
 (defun cos5--unibyte-string-to-hex (s)
+  "Convert unibyte string S to hex."
   (mapconcat (lambda (char) (format "%02x" char)) s ""))
 
 (defun cos5--hmac-sha1 (key value)
+  "Compute HMAC-SHA1(KEY, VALUE) and return the hex form."
   (cos5--unibyte-string-to-hex (gnutls-hash-mac 'SHA1 key value)))
 
 (defun cos5--KeyTime (duration)
+  "Return a keytime, the key is valid for next DURATION seconds."
   (let ((StartTimestamp (format-time-string "%s"))
         (EndTimestamp (format-time-string "%s" (time-add nil duration))))
     (format "%s;%s" StartTimestamp EndTimestamp)))
@@ -51,6 +54,13 @@
                    HttpURI
                    HttpParameters
                    HttpHeaders)
+  "Sign the request.
+According to
+\(KEYTIME SECRETID SECRETKEY HTTPMETHOD HTTPURI HTTPPARAMETERS HTTPHEADERS).
+
+HTTPPARAMETERS and HTTPHEADERS are alist.
+
+See URL `https://cloud.tencent.com/document/product/436/7778'."
   (let* ((SignKey (cos5--hmac-sha1 SecretKey KeyTime))
          (HttpParameters (cl-sort
                           (mapcar
